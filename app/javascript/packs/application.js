b2f720428357486dcb863f3760a783a850e5a92e
kkrasilov <kkrasilov@gmail.com>
2020-08-23 17:51:00 +0500

    add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 require("@rails/ujs").start()
+import 'bootstrap/dist/js/bootstrap'
 
-const images = require.context('../images', true)
+const images = require.context('../../assets/images', true)
 
