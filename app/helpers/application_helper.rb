67d56a53d884a22eacad58d3c3a165d5414a6179
kkrasilov <kkrasilov@gmail.com>
2020-09-17 06:29:49 +0500

    add photo to events

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,34 @@ module ApplicationHelper
     end
   end
 
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_pack_path('media/images/user.png')
+    end
+  end
+
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_pack_path('media/images/event.jpg')
+    end
+  end
+
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_pack_path('media/images/event_thumb.jpg')
+    end
+  end
+
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class} text-danger"
   end
