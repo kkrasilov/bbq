f7e86cc363ab8dc8c35a3cb86f5511d35699a223
kkrasilov <kkrasilov@gmail.com>
2020-09-20 09:21:06 +0500

    add devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,4 +51,13 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: '587',
+    user_name: 'partybbq', # не используйте для тестов свои реальные ящики
+    password: '666',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
